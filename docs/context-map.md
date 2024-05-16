0d7874a0c2fecfcf317b09dd9fc30abab12847bb
= <wellyson.freitas@n26.com>
2024-05-16 22:39:58 +0200

    Setup infra

--- docs/context-map.md
+++
@@ -1,5 +0,0 @@
-# Mapa de Contexto
-
-![Mapa de Contexto](diagrams/context-map.png)
-
-Source: [Miro whiteboard público ↗️](https://miro.com/app/board/uXjVMqdH21Q=/?share_link_id=46485218697)
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,11 @@
-variable "account_id" {
-  type = string
-}
-
 variable "region" {
   type = string
-}
-
-variable "website_bucket_name" {
-  type = string
+  default = "us-east-1"
 }
 
 variable "tags" {
   type = map(string)
+  default = {
+    managed_by_terraform = true
+  }
 }
