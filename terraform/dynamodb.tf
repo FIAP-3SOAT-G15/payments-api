84556213bde12d9c0b03695964c0841b5b1af9c3
Wellyson Freitas <wellyfrs@gmail.com>
2024-05-17 15:45:18 +0200

    Config param

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,16 +1,21 @@
 module "dynamodb_table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "4.0.1"
-  
-  name = "payments"
-  hash_key = "payment_order_number"
+
+  name        = "payments"
+  hash_key    = "payment_order_number"
+  range_key   = "payment_created_at"
   table_class = "STANDARD"
-  
+
   attributes = [
     {
       name = "payment_order_number"
       type = "S"
     },
+    {
+      name = "payment_created_at"
+      type = "S"
+    }
   ]
 
   tags = var.tags
